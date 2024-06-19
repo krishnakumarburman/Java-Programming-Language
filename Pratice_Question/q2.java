c4a9bf8645ac8d06fbdf503b3355be2148969341
Krishna Kumar Burman <krishnakumarburmanid@gmail.com>
2024-06-20 01:00:15 +0530

    questions solving

---
+++ Pratice_Question/q2.java
@@ -0,0 +1,12 @@
+package Pratice_Question;
+import java.util.*;
+
+public class q2 {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Give the length of side of a square = ");
+        int side = sc.nextInt();
+        System.out.println("are of give length square is = " + (side * side) );
+        sc.close();
+    }
+}
---
+++ Pratice_Question/q3.java
@@ -0,0 +1,19 @@
+package Pratice_Question;
+
+import java.util.Scanner;
+
+public class q3 {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("price of pencile = ");
+        Float pencile = sc.nextFloat();
+        System.out.print("price of pen = ");
+        Float pen = sc.nextFloat();
+        System.out.print("price of eraser = ");
+        float eraser = sc.nextFloat();
+        System.out.println("Your Total Bill (exclusive 18% GST) = " + (pencile+pen+eraser));
+        Float TotalGST = ((pencile+pen+eraser)*18)/100;
+        System.out.println("Your Total Bill (inclusive 18% GST) = " + (pencile+pen+eraser+TotalGST));
+        sc.close();
+    }
+}
