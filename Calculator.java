53c454484fae4938cab9357d2fd30c84064a237f
Krishna Kumar Burman <krishnakumarburmanid@gmail.com>
2024-08-30 16:28:15 +0530

    calculator using Swich statements

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Calculator {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        char operator = sc.next().charAt(0); // sc.next() take string type of value but we need char value so we use this , we will learn more about it in string subject.
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        switch(operator){
+
+            case '+' : System.out.println(a+b);
+                    break;
+            case '-' : System.out.println(a-b);
+                    break;
+            case '*' : System.out.println(a*b);
+                    break;
+            case '/' : System.out.println(a/b);
+                    break;
+            case '%' : System.out.println(a%b);
+                    break;
+            default : System.out.println("Wrong Operator");
+        }
+    }
+}
--- Swich.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-
-public class Swich{
-    public static void main(String arg[]){
-        
-    }
-}
