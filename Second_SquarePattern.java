628c9f68543b1c59d9017e13d0ca78a397a2cacf
Krishna Kumar Burman <krishnakumarburmanid@gmail.com>
2024-08-31 17:46:43 +0530

    second Square matrix pattern

---
+++ Second_SquarePattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Second_SquarePattern {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1 ; i <=n ; i++){
+            for (int j = 1 ; j <= (n-i+1) ; j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
